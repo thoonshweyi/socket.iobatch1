dbe5b565b89a9f9937a17300bcb4c010ee9fb0ab
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2026-04-20 21:24:59 +0630

    Typing by NS

---
+++ l17exerciseroombasicbynamespaceuserscount/data/namespace.js
@@ -0,0 +1,20 @@
+export const namespaces = [
+    {
+        id: 0,
+        name: "Laravel",
+        endpoint:"/laravel",
+        description: "Discussions about Laravel articles"
+    },
+    {
+        id: 1,
+        name: "Reactjs",
+        endpoint:"/reactjs",
+        description: "Talka about Reactjs and developer tools."
+    },
+    {
+        id: 2,
+        name: "Python",
+        endpoint:"/python",
+        description: "All about Python Language"
+    },
+]
