cc428985bcf06e88fefa13062abc95e1ee1f2a93
Thoon Shwe Yi Kyaw <thoonlay779@gmail.com>
2026-06-04 22:17:03 +0630

    4JUN

--- l22exerciseroomanddashboard/server.js
+++ l22exerciseroomanddashboard/server.js
@@ -36,23 +36,123 @@ const io = new Server(server,{
 });
 
 
-const rooms = ["Sales","Marketing","HR","Warehouse","IT"];
-// Start Helper function
+const initrooms = ["Sales","Marketing","HR","Warehouse","IT"];
 
+// Start Helper function
+function getOnlineUsers(){
+    // const users = [];
+
+    // namespaceInstance.sockets.forEach((socket)=>{
+    //     users.push({
+    //         id:socket.id,
+    //         name: socket.data.userName || `User-${socket.id.slice(0,4)}`,
+    //         room: socket.data.currentRoom || null
+    //     })
+    // })
+
+    // return users;
+}
+
+function emitOnlineUsers(){
+    io.emit('onlineUsersFromServer',getOnlineUsers());
+}
 // End Helper funtion
 
 io.on("connection",(socket)=>{
     console.log("Connected: ", socket.id);
 
-    socket.data.username = `User-${socket.id.slice(0,4)}`;
+    socket.data.userName = `User-${socket.id.slice(0,4)}`;
     socket.data.currentRoom = null;
     
     // send room list
-    socket.emit('roomList',rooms);
+    socket.emit('roomList',initrooms);
 
     // Set profile
+    socket.on("setProfileFromClient",(data,callback)=>{
+        const name = String(data?.name ?? "").trim().slice(0,20);
+
+        if(!name){
+            return callback?.({
+                ok: false,
+                message: "Name is required"
+            })
+        }
+
+        socket.data.userName = name;
+        callback?.({
+            ok: true,
+            name,
+            message: "Name Updated!"
+        });
+
+        // Online room users count 
+        emitOnlineUsers();
+    });
 
     // Join room
+        socket.on("joinRoomFromClient",(roomName,callback)=>{
+            try{
+
+                roomName = String(roomName || "").trim();
+                if(!roomName){   
+                    return callback?.({
+                        ok: false,
+                        error: "Room Name required."
+                    });
+                }
+
+                if(!initrooms.includes(roomName)){   
+                    return callback?.({
+                        ok: false,
+                        error: "Invalid room name."
+                    });
+                }
+
+                const prevRoom = socket.data.currentRoom;
+
+                // leave old room
+                if(prevRoom && prevRoom !== roomName){
+
+                    socket.leave(prevRoom);
+
+                    emitRoomCount();
+
+                    socket.to(prevRoom).emit('systemMessageFromServer',{
+                        message: `${socket.data.userName} left ${prevRoom}`
+                    });
+
+                }
+
+                
+                // join new room
+                if(socket.data.currentRoom !== roomName){
+                    socket.join(roomName);
+                    socket.data.currentRoom = roomName;
+                }
+                
+                // update room user count
+                emitRoomCount();
+
+                // notify others in room
+                socket.to(roomName).emit('systemMessageFromServer',{
+                    message: `${socket.data.userName} joined ${roomName}`
+                });
+
+                // obj for response
+                callback?.({
+                    ok: true,
+                    roomname: roomName,
+                    message: `Joined ${room}`
+                });
+
+            }catch(err){
+                console.error("joinRoomFromClient error: ",err)
+                callback?.({
+                    ok: false,
+                    error: "Joined Failed."
+                })
+            }
+        })
         // leave old room
 
         // join new room
@@ -60,10 +160,96 @@ io.on("connection",(socket)=>{
         // callback
 
     // chat Message
+    socket.on("messageFromClient",(data,callback)=>{
+        const room = socket.data.currentRoom;
+        const username = socket.data.userName;
+        if(!room){
+            return callback({ok:false, message: "Join a foom first"});
+        }
+
+        const text = String(data?.getinputval ?? "").trim();
+
+        if(!text){
+            return callback({ok:false,message: "Empty message"});
+        }
+
+        // server to room client for display
+        io.to(room).emit('messageFromServer',{
+            room,
+            from: username,
+            text: data.getinputval,
+            at: new Date().toLocaleTimeString()
+        });
+
+        callback({ok:true});
+
+    });
+
+
+    // Admin Announcement Message
+    socket.on("adminmessageFromClient",(data,callback)=>{
+        const password = String(data?.getinputpassword ?? "").trim();
+        const text = String(data?.getinputval ?? "").trim();
+
+        if(password !== 'admin123'){
+            return callback({ok:false,message: "Wrong admin password"});
+        }
+
+        if(!text){
+            return callback({ok:false,message: "Announcement message is required"});
+        }
+
+        // server to room client for display
+        io.emit('adminmessageFromServer',{
+            text,
+            at: new Date().toLocaleTimeString()
+        });
+
+        callback({ok:true,message: "Announcement sent."});
+
+    });
 
     // Typing
+    // Typing from client
+    socket.on("typingFromClient",()=>{
+        const room = socket.data.currentRoom;
+        if(!room) return;
+
+        socket.to(room).emit("typingFromServerNS",{
+            from: socket.data.userName
+        });
+    });
+
+    // Stop typing from client
+    socket.on("stopTypingFromClientNS",()=>{
+        const room = socket.data.currentRoom;
+        if(!room) return;
+
+        socket.to(room).emit("stoptypingFromServerNS",{
+            from: socket.data.userName
+        });
+    });
 
     // Disconect
+     
+    socket.on('disconnect',(reason)=>{
+        console.log(`Client ${socket.id} disconnected. Reason: `, reason);
+
+        // after disconnect, send system message
+        const room = socket.data.currentRoom;
+        const username = socket.data.userName;
+
+        if(room){
+            socket.to(room).emit("systemMessageFromServer",{
+                message: `${username} disconnected.`
+            })
+        }
+    
+        // remove typing indicator for disconnected user
+        socket.broadcast.emit("stoptypingFromServerNS",{
+            from: socket.data.userName
+        });
+    });
 });
 
 
